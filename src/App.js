8e322d81ece4077e7dc3a0b9a43ec5d9d37ae457
jewelCode <jewelbarua101@gmail.com>
2022-05-22 21:40:40 +0600

    firebase and tailwind configuration complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
+import SignUp from './Pages/Login/SignUp';
+import Purchase from './Pages/Purchase/Purchase';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-        <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+        <Navbar></Navbar>
+        <Routes>
+              <Route path="/" element={<Home></Home>}></Route>
+              <Route path="login" element={<Login></Login>}></Route>
+              <Route path="signup" element={<SignUp></SignUp>}></Route>
+              <Route path="purchase" element={<RequireAuth>
+                      <Purchase></Purchase>
+              </RequireAuth>}></Route>
+        </Routes>
     </div>
   );
 }
---
+++ src/Pages/Purchase/Purchase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Purchase = () => {
+    return (
+        <div>
+            <h1>This is Purchase Protected Page</h1>
+        </div>
+    );
+};
+
+export default Purchase;
